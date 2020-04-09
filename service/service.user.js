497be8da3c6b8fdf065b4d6c6d885e177ddcafae
NewDivide41194 <heinhtet25944@gmail.com>
2020-04-09 18:23:22 +0630

    survey4/9/2020UPDATE@HMT

--- service/service.user.js
+++ service/service.user.js
@@ -4,14 +4,18 @@ const getAdmin = () => {
     return surveydb.getAdmin();
 }
 
-const addUser = (userName, password, email) => {
-    return surveydb.addUser(userName, password, email)
+const addCompany = (companyName) => {
+    return surveydb.addCompany(companyName)
+}
+
+const addUser = (userName, password, email, companyName) => {
+    return surveydb.addUser(userName, password, email, companyName)
 }
 
 // @HMH
 
-const updateUser = (userId, userName, password, email) => {
-    return surveydb.updateUser(userId, userName, password, email)
+const updateUser = (userId, userName, password, email, companyId) => {
+    return surveydb.updateUser(userId, userName, password, email, companyId)
 }
 
 const checkDuplicateEmailInsert = (email) => {
@@ -20,6 +24,10 @@ const checkDuplicateEmailInsert = (email) => {
 
 const checkDuplicateEmailUpdate = (email, user_id) => {
     return surveydb.checkDuplicateEmailUpdate(email, user_id);
+}
+
+const getCompany = () => {
+    return surveydb.getCompany();
 }
 
 // const addAdmin = (username, password, active, employeeId) => {
@@ -34,5 +42,5 @@ const checkDuplicateEmailUpdate = (email, user_id) => {
 //     return surveydb.updateAdmin(userId,username, password, active, employeeId);
 // }
 
-module.exports = { getAdmin, addUser, checkDuplicateEmailInsert,checkDuplicateEmailUpdate, updateUser }
+module.exports = { getAdmin, addUser, checkDuplicateEmailInsert, checkDuplicateEmailUpdate, updateUser, getCompany, addCompany }
 // ,addAdmin,updateAdmin,getAdminById
